a4948dd62abb941a76c05c74eaa58fb81fa2a3fb
infinitY0369 <flutist.sud.0v@icloud.com>
2024-02-21 03:43:53 +0900

    Fix PCWSTR encoding.

--- mods/pak-natives/src/lib.rs
+++ mods/pak-natives/src/lib.rs
@@ -4,10 +4,13 @@ mod utils;
 
 use pak::Pak;
 use std::{ffi::c_void, panic};
-use windows_sys::Win32::{
-    Foundation::{BOOL, HINSTANCE, TRUE},
-    System::{LibraryLoader::DisableThreadLibraryCalls, SystemServices::DLL_PROCESS_ATTACH},
-    UI::WindowsAndMessaging::{MessageBoxW, IDCANCEL, MB_ICONERROR, MB_OKCANCEL},
+use windows::{
+    core::{self, PCWSTR},
+    Win32::{
+        Foundation::{BOOL, HINSTANCE, TRUE},
+        System::{LibraryLoader::DisableThreadLibraryCalls, SystemServices::DLL_PROCESS_ATTACH},
+        UI::WindowsAndMessaging::{MessageBoxW, IDCANCEL, MB_ICONERROR, MB_OKCANCEL},
+    },
 };
 
 #[allow(clippy::upper_case_acronyms)]
@@ -18,18 +21,18 @@ type LPVOID = *const c_void;
 #[no_mangle]
 pub extern "stdcall" fn DllMain(handle: HINSTANCE, reason: DWORD, _reserved: LPVOID) -> BOOL {
     unsafe {
-        DisableThreadLibraryCalls(handle);
+        let _ = DisableThreadLibraryCalls(handle);
     };
 
     if reason == DLL_PROCESS_ATTACH {
         match Pak::create() {
             Ok(_) => {}
             Err(e) => {
-                let caption = "PakNatives\0".encode_utf16().collect::<Vec<u16>>().as_ptr();
-                let txt = (e.to_string() + "\0").as_str().encode_utf16().collect::<Vec<u16>>().as_ptr();
+                let txt = PCWSTR(e.to_string().encode_utf16().chain([0u16]).collect::<Vec<u16>>().as_ptr());
+                let caption = core::w!("PakNatives");
                 let utype = MB_OKCANCEL | MB_ICONERROR;
 
-                if unsafe { MessageBoxW(0, txt, caption, utype) } == IDCANCEL {
+                if unsafe { MessageBoxW(None, txt, caption, utype) } == IDCANCEL {
                     panic!();
                 }
             }
