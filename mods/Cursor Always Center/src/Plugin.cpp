ba32632fbc8142b95cdff5d8645e817387544ca1
infinitY0369 <flutist.sud.0v@icloud.com>
2024-02-21 01:44:56 +0900

    Update `Cursor Always Center`.

--- mods/Cursor Always Center/src/Plugin.cpp
+++ mods/Cursor Always Center/src/Plugin.cpp
@@ -1,34 +1,6 @@
-#include <bit>
 #include <reframework/API.hpp>
 
-int pre_fn(int argc, void **argv, REFrameworkTypeDefinitionHandle *arg_tys, unsigned long long ret_addr)
-{
-    auto vm_context = reframework::API::get()->get_vm_context();
-
-    const auto scene_manager = reframework::API::get()->get_native_singleton("via.SceneManager");
-    const auto scene_manager_type = reframework::API::get()->tdb()->find_type("via.SceneManager");
-
-    const auto get_main_view = scene_manager_type->find_method("get_MainView()");
-    const auto main_view = get_main_view->call<reframework::API::ManagedObject *>(vm_context, scene_manager);
-
-    auto get_window_size_invoke_result = main_view->invoke("get_WindowSize()", {});
-    float *size_invoke = (float *)&get_window_size_invoke_result;
-
-    long x = std::bit_cast<long>(size_invoke[0] / 2);
-    long y = std::bit_cast<long>(size_invoke[1] / 2);
-
-    long long xy = (static_cast<long long>(y) << 0x20) | (x & 0xFFFFFFFF);
-
-    const auto obj = (reframework::API::ManagedObject *)argv[1];
-    *obj->get_field<long long>("_mos_BackupViewCursorPosition") = xy;
-    *obj->get_field<bool>("_mos_Backup_isMouseCursor") = true;
-
-    return REFRAMEWORK_HOOK_SKIP_ORIGINAL;
-}
-
-void post_fn(void **ret_val, REFrameworkTypeDefinitionHandle ret_ty, unsigned long long ret_addr)
-{
-}
+using namespace reframework;
 
 extern "C" __declspec(dllexport) void reframework_plugin_required_version(REFrameworkPluginVersion *version)
 {
@@ -41,11 +13,44 @@ extern "C" __declspec(dllexport) void reframework_plugin_required_version(REFram
 
 extern "C" __declspec(dllexport) bool reframework_plugin_initialize(const REFrameworkPluginInitializeParam *param)
 {
-    reframework::API::initialize(param);
-    reframework::API::get()
+    API::initialize(param);
+
+    API::get()
         ->tdb()
         ->find_method("snow.StmInputManager.InGameInputDevice", "backupMouseCursorPosition()")
-        ->add_hook(pre_fn, post_fn, false);
+        ->add_hook(
+            [](int /* argc */, void **argv, REFrameworkTypeDefinitionHandle * /* arg_tys */, unsigned long long /* ret_addr */) -> int {
+                const auto &api = API::get();
+                auto *const tdb = api->tdb();
+
+                auto *const vmctx = static_cast<API::VMContext *>(argv[0]);
+                auto *const obj = static_cast<API::ManagedObject *>(argv[1]);
+
+                static auto *const this_t = obj->get_type_definition();
+                static auto *const scene_manager = tdb->find_type("via.SceneManager");
+                static auto *const scene_view = tdb->find_type("via.SceneView");
+
+                static auto *const get_main_view = scene_manager->find_method("get_MainView()");
+                static auto *const get_window_size = scene_view->find_method("get_WindowSize()");
+
+                static auto *backup_view_cursor_position = this_t->find_field("_mos_BackupViewCursorPosition");
+                static auto *backup_is_mouse_cursor = this_t->find_field("_mos_Backup_isMouseCursor");
+
+                auto *const main_view = get_main_view->call<API::ManagedObject *>(vmctx, nullptr);
+
+                std::array<float, 2> window_size{};
+                get_window_size->call(&window_size, vmctx, main_view);
+
+                std::array<float, 2> pos{};
+                pos[0] = window_size[0] / 2.0F;
+                pos[1] = window_size[1] / 2.0F;
+
+                backup_view_cursor_position->get_data<std::array<float, 2>>(obj) = pos;
+                backup_is_mouse_cursor->get_data<bool>(obj) = true;
+
+                return REFRAMEWORK_HOOK_SKIP_ORIGINAL;
+            },
+            [](void ** /* ret_val */, REFrameworkTypeDefinitionHandle /* ret_ty */, unsigned long long /* ret_addr */) -> void {}, false);
 
     return true;
 }
