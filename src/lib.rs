4fe290d4158400dc23f4757986c85154fba4defb
Torvosaure <32787487+Torvosaure@users.noreply.github.com>
2025-07-20 10:46:05 +0200

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+mod pak;
+mod structure;
+mod utils;
+
+
+use pak::Pak;
+use std::{ffi::c_void, process};
+use windows::{
+    core::{self, PCWSTR},
+    Win32::{
+        Foundation::{BOOL, HINSTANCE, TRUE},
+        System::{LibraryLoader::DisableThreadLibraryCalls, SystemServices::DLL_PROCESS_ATTACH},
+        UI::WindowsAndMessaging::{MessageBoxW, IDCANCEL, MB_ICONERROR, MB_OKCANCEL},
+    },
+};
+
+#[allow(clippy::upper_case_acronyms)]
+type DWORD = u32;
+#[allow(clippy::upper_case_acronyms)]
+type LPVOID = *const c_void;
+
+#[unsafe(no_mangle)]
+pub extern "stdcall" fn DllMain(handle: HINSTANCE, reason: DWORD, _reserved: LPVOID) -> BOOL {
+    unsafe {
+        let _ = DisableThreadLibraryCalls(handle);
+    };
+
+    if reason == DLL_PROCESS_ATTACH {
+        match Pak::create() {
+            Ok(_) => {}
+            Err(e) => {
+                let txt = PCWSTR(e.to_string().encode_utf16().chain([0u16]).collect::<Vec<u16>>().as_ptr());
+                let caption = core::w!("PakNatives");
+                let utype = MB_OKCANCEL | MB_ICONERROR;
+
+                if unsafe { MessageBoxW(None, txt, caption, utype) } == IDCANCEL {
+                    process::exit(1);
+                }
+            }
+        }
+    }
+
+    TRUE
+}
