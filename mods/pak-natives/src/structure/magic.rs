88e62fd867d82eaa0ea10eca9e017ec8ae4c779c
Torvosaure <32787487+Torvosaure@users.noreply.github.com>
2025-07-20 10:40:07 +0200

    Delete mods directory

--- mods/pak-natives/src/structure/magic.rs
+++
@@ -1,7 +0,0 @@
-pub struct Magic {}
-
-impl Magic {
-    pub const PAK: &'static [u8; 4] = b"KPKA";
-    pub const BNK: &'static [u8; 4] = b"BKHD";
-    pub const PCK: &'static [u8; 4] = b"AKPK";
-}
--- mods/pak-natives/src/structure/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod file_entry;
-pub mod header;
-pub mod magic;
-
-pub enum CompressType {
-    Store,
-    Deflate,
-    Zstd,
-}
